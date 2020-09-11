80760cd021e7c306a945b7a20ebdf21c0e4118ac
Dillikumar <dillikumar270399@gmail.com>
2020-09-11 19:29:30 +0530

    Updates

--- src/components/FontawesomeIcons/index.js
+++
@@ -1,4 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {faFileAlt,faArrowLeft ,faChevronRight,faFileWord,faUser,faUserFriends,faEnvelope,faChevronDown,faBrain,faNewspaper,faBook, faMicrophone, faCamera, faVideo, faBars, faTimes} from '@fortawesome/free-solid-svg-icons';
- 
-library.add(faFileAlt,faArrowLeft,faChevronRight,faFileWord,faUser,faUserFriends,faEnvelope,faChevronDown,faBrain,faNewspaper,faBook,faMicrophone,faCamera,faVideo,faBars,faTimes);
